ee50cd007534c51f9d4e5cf7a5e292978f157560
jhs512 <jangka512@gmail.com>
2025-05-23 10:17:49 +0900

    work

--- src/main/java/com/back/Main.java
+++ src/main/java/com/back/Main.java
@@ -8,6 +8,8 @@ public class Main {
 
         Scanner scanner = new Scanner(System.in);
 
+        int lastId = 0;
+
         while (true) {
             System.out.print("명령) ");
             String cmd = scanner.nextLine().trim();
@@ -20,7 +22,9 @@ public class Main {
                 System.out.print("작가 : ");
                 String wiseSayingAuthor = scanner.nextLine().trim();
 
-                System.out.println("1번 명언이 등록되었습니다.");
+                int no = ++lastId;
+
+                System.out.println("%d번 명언이 등록되었습니다.".formatted(no));
             }
         }
 
